0cab97a7feae673e5594f7e0419683a1226854ca
Morgane Delmas <mdelmas93@gmail.com>
2023-08-03 15:56:08 +0200

    Part 1: exercises 1.12 to 1.14

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+const Title = ({ text }) => <h1>{text}</h1>
+
+const Button = ({ text, handleClick }) => (<button onClick={handleClick}>{text}</button>)
+
+const Anecdote = ({ text, votes }) => (
+  <p>{ text }<br />
+  has { votes } votes</p>
+)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  const selectRandomAnecdote = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+  const handleVote = () => {
+    const newVotes = [...votes]
+    newVotes[selected]++
+    setVotes(newVotes)
+  }
+
+  const maxVotes = Math.max(...votes)
+  const mostVotedAnecdote = votes.indexOf(maxVotes)
+
+  return (
+    <div>
+      <Title text="Anecdote of the day" />
+      <Anecdote text={anecdotes[selected]} votes={votes[selected]} />
+
+      <Button text="vote" handleClick={handleVote} />
+      <Button text="next anecdote" handleClick={selectRandomAnecdote} />
+
+      {maxVotes !== 0 ? 
+        <>
+          <Title text="Anecdote with most votes" />
+          <Anecdote text={anecdotes[mostVotedAnecdote]} votes={votes[mostVotedAnecdote]} />      
+        </> 
+        : <Title text="No votes yet" />
+      }
+    </div>
+  )
+}
+
+export default App
